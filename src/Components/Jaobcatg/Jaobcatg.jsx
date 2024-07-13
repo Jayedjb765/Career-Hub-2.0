ca20f0e36f32f1c21e2321d209c0cbedc46af867
Jayedjb765 <jayedjb76@gmail.com>
2024-07-13 19:51:42 +0600

    adding Job Category List

--- src/Components/Jaobcatg/Jaobcatg.jsx
+++ src/Components/Jaobcatg/Jaobcatg.jsx
@@ -1,13 +1,53 @@
-
+import acc from '../../assets/assets/icons/accounts.png'
+import bsc from '../../assets/assets/icons/creative.png'
+import scl from '../../assets/assets/icons/marketing.png'
+import chip from '../../assets/assets/icons/chip.png'
 
 const Jaobcatg = () => {
-    return (
-        <div className="my-14">
-            <h3 className="text-5xl font-bold text-center">Job Category List</h3>
-            <h5 className="text-base text-center mt-3">Explore thousands of job opportunities with all the information you need. Its your future</h5>
-            
+  return (
+    <div className="my-14">
+      <h3 className="text-5xl font-bold text-center">Job Category List</h3>
+      <h5 className="text-base text-center mt-3">
+        Explore thousands of job opportunities with all the information you
+        need. Its your future
+      </h5>
+     <div className='grid grid-cols-4 gap-2 mt-5'>
+     <div className="card bg-gradient-to-r from-[#7E90FE0D] to-[#9873FF0D] shadow-xl">
+       <img src={acc} className='w-[50px] ml-8 mt-4' alt="" />
+       
+        <div className="card-body  ">
+          <h2 className="card-title">Account & Finance</h2>
+          <p>300 Jobs Available</p>
+          
         </div>
-    );
+      </div>
+     <div className="card bg-gradient-to-r from-[#7E90FE0D] to-[#9873FF0D] shadow-xl">
+     <img src={bsc} className='w-[50px] ml-8 mt-4' alt="" />
+        <div className="card-body  ">
+          <h2 className="card-title">Account & Finance</h2>
+          <p>300 Jobs Available</p>
+          
+        </div>
+      </div>
+     <div className="card bg-gradient-to-r from-[#7E90FE0D] to-[#9873FF0D] shadow-xl">
+        <img src={scl} className='w-[50px] ml-8 mt-4' alt="" />
+        <div className="card-body  ">
+          <h2 className="card-title">Account & Finance</h2>
+          <p>300 Jobs Available</p>
+          
+        </div>
+      </div>
+     <div className="card bg-gradient-to-r from-[#7E90FE0D] to-[#9873FF0D] shadow-xl">
+     <img src={chip} className='w-[50px] ml-8 mt-4' alt="" />  
+        <div className="card-body  ">
+          <h2 className="card-title">Account & Finance</h2>
+          <p>300 Jobs Available</p>
+          
+        </div>
+      </div>
+     </div>
+    </div>
+  );
 };
 
 export default Jaobcatg;
