6dda65d7ddbcfb529717bc9a6ac1c2f5221a329e
Jayedjb765 <jayedjb76@gmail.com>
2024-07-13 18:38:11 +0600

    Complete header

--- src/Components/Root/Root.jsx
+++ src/Components/Root/Root.jsx
@@ -1,8 +1,14 @@
+import { Outlet } from "react-router-dom";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
 
 
 const Root = () => {
     return (
-        <div>
+        <div className="max-w-7xl mx-auto">
+            <Header></Header>
+            <Outlet></Outlet>
+            <Footer></Footer>
             
         </div>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,28 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Root from './Components/Root/Root';
+import Home from './Components/Home/Home';
+import Jobs from './Components/Jobs/Jobs';
+import Blogs from './Components/Blogs/Blogs';
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root></Root>,
+    children : [
+      {
+        path: "/",
+        element : <Home></Home>
+      },
+      {
+        path: "/jobs",
+        element: <Jobs></Jobs>
+      },
+      {
+        path :"/blogs",
+        element: <Blogs></Blogs>
+      }
+    ]
   },
 ]);
 ReactDOM.createRoot(document.getElementById('root')).render(
